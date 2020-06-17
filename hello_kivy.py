f70cf7a821c28fe33ae3744d1c9d3fb13f34351f
Daniel <coimbrasantos.d@gmail.com>
2020-06-16 21:25:03 -0300

    Hello World App

---
+++ hello_kivy.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+
+import kivy
+kivy.require('1.0.7')
+
+from kivy.app import App
+from kivy.uix.button import Button
+
+
+class TestApp(App):
+
+    def build(self):
+        # return a Button() as a root widget
+        return Button(text='hello world')
+
+
+if __name__ == '__main__':
+    TestApp().run()
